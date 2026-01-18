e3a765fee084872695ca9839f852062dcdeac2f9
suryans77 <suryansdash005@gmail.com>
2026-01-18 13:55:57 +0530

    tuned robot

---
+++ controllers/e-puck_avoid_obstacles_plus/build/release/e-puck_avoid_obstacles_plus.d
@@ -0,0 +1,10 @@
+build/release/e-puck_avoid_obstacles_plus.o: \
+ e-puck_avoid_obstacles_plus.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/device.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/led.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h
--- controllers/e_puck_avoid_obstacles/build/release/e_puck_avoid_obstacles.d
+++
@@ -1,7 +0,0 @@
-build/release/e_puck_avoid_obstacles.o: e_puck_avoid_obstacles.c \
- C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/led.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
